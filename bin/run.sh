66e4c3d9f591de44702baa80e0294336eaaf823f
zmx <2240564630@qq.com>
2022-04-07 00:01:40 +0800

    4月7日

---
+++ bin/run.sh
@@ -0,0 +1,32 @@
+#！/bin/sh
+echo -e "create simulate data\n"
+./create_seq 20
+echo -e "map  the accurate sub_seq to the mis_seq by using blasr\n"
+
+echo -e "command：\n"
+echo -e "blasr   ./../sub_seq/sub_seq.fasta\n
+        ./../mis_seq/mis_seq.fasta\n
+        --header\n
+        --bestn 1\n
+        -m 5 --out\n
+        ./../map_result/map_result.out\n"
+blasr   ./../sub_seq/sub_seq.fasta\
+        ./../mis_seq/mis_seq.fasta\
+        --header\
+        --bestn 1\
+        -m 5 --out\
+        ./../map_result/map_result.out
+
+echo -e "\ncorrect the mis_seq according to the map result\n"
+./correct
+
+echo -e "get the similarity between the init_seq and mis_seq and the similarity between the init_seq and corrected_seq\n"
+
+./compare
+echo -e "\ncount the microsatellite of the corrected_seq\n"
+./mic_count
+echo -e "count result:\n"
+while read line
+do
+echo $line
+done<./../result/result.out
